e79336f8acd3336afe753e40c32c152e8a133860
diegomoreno <diego.moreno@linx.com.br>
2020-01-31 20:14:29 -0300

    feat(criando loader de models): criando loader de Models
    Criando loader de Models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
-const { Router } = require("express");
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Moreno',
+    email: 'diegovalemoreno@gmail.com',
+    password_hash: '1232131',
+  });
+  return res.json(user);
 });
+
 
 module.exports = routes;
